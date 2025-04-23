a8a0585758a643a66152dc0e3081f2bd8155859b
陈生杂物房 <88823709+TC999@users.noreply.github.com>
2025-04-23 14:32:27 +0800

    修改参数

--- download.sh
+++ download.sh
@@ -4,6 +4,7 @@
 OWNER="TC999"
 REPO="CHJSL-archive"
 TAG=$1  # 传入的 tag 参数
+FILENAME=$2
 
 # 获取发行版信息
 API_URL="https://api.github.com/repos/$OWNER/$REPO/releases/tags/$TAG"
@@ -13,7 +14,7 @@ ASSETS=$(curl -s $API_URL | grep "browser_download_url" | grep "\.mp4" | cut -d 
 for URL in $ASSETS; do
   FILENAME=$(basename "$URL")
   echo "正在下载 $FILENAME..."
-  curl -L "$URL" -o 1.mp4
+  curl -L "$URL" -o "$FILENAME"
 done
 
 echo "完成！"
