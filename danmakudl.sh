9a099161766bac9b103b93abfe7c43771ba848d9
陈生杂物房 <88823709+TC999@users.noreply.github.com>
2025-09-24 11:34:59 +0800

    新增：GA 弹幕去重

---
+++ danmakudl.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 仓库信息
+OWNER="TC999"
+REPO="CHJSL-archive"
+TAG=$1  # 传入的 tag 参数
+OUTPUT_NAME=$2  # 传入的输出文件名参数
+
+# 获取发行版信息
+API_URL="https://api.github.com/repos/$OWNER/$REPO/releases/tags/$TAG"
+ASSETS=$(curl -s $API_URL | grep "browser_download_url" | grep "\.ass" | cut -d '"' -f 4)
+
+# 下载 .ass 文件
+for URL in $ASSETS; do
+  FILENAME=$(basename "$URL")
+  echo "正在下载 $FILENAME..."
+  curl -L "$URL" -o "$OUTPUT_NAME"
+done
+
+echo "完成！"
